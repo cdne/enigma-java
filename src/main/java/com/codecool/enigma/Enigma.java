b72b92ee5c1e2ab8dae1cdc46036692845e2f007
Claudiu Duneata <cduneata@gmail.com>
2021-10-26 17:01:26 +0300

    Implement Enigma application

--- src/main/java/com/codecool/enigma/Enigma.java
+++ src/main/java/com/codecool/enigma/Enigma.java
@@ -1,5 +1,9 @@
 package com.codecool.enigma;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 class Enigma {
 
     private static String MENU = "Enigma Manual\n" +
@@ -11,14 +15,24 @@ class Enigma {
             "   FileName        : path to file to encrypt/decrypt\n" +
             "   EncryptionKey   : Optional -> must be provided if cipher requires a key";
 
-    public static void main(String[] args) throws EnigmaException {
+    public static void main(String[] args) throws EnigmaException, IOException {
         ArgsParser argsParser = new ArgsParser(args);
         handleCipherOperation(argsParser);
     }
 
-    private static void handleCipherOperation(ArgsParser argsParser) throws EnigmaException {
-        Cipher cipher = CipherFactory.getCipherForArgs(argsParser);
-        // use cipher
+    private static void handleCipherOperation(ArgsParser argsParser) throws EnigmaException, IOException {
+        if(CipherFactory.isCipherAvailable(argsParser.cipher)){
+            Cipher cipher = CipherFactory.getCipherForArgs(argsParser);
+            HandleFiles filesHandler = new HandleFiles();
+            if(argsParser.option.equalsIgnoreCase("-e")){
+                System.out.println(cipher.encrypt(filesHandler.getFileText(argsParser.file)));
+            } else if(argsParser.option.equalsIgnoreCase("-d")) {
+                System.out.println(cipher.decrypt(filesHandler.getFileText(argsParser.file)));
+            } else if(argsParser.option.equalsIgnoreCase("-h")) {
+                System.out.println(MENU);
+            }
+        }
+
     }
 
 }
