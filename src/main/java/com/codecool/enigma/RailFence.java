45aa3cbc08d02ab000cfd4e25f0cc6fb0364fa9b
Claudiu Duneata <cduneata@gmail.com>
2021-10-26 17:00:47 +0300

    Refactor rail-fence implementation to take a key

--- src/main/java/com/codecool/enigma/RailFence.java
+++ src/main/java/com/codecool/enigma/RailFence.java
@@ -2,17 +2,23 @@ package com.codecool.enigma;
 
 class RailFence implements Cipher {
 
+    private int key;
+
+    RailFence(String key) {
+        this.key = Integer.parseInt(key);
+    }
+
     @Override
     public String encrypt(String message) {
         String encryptedText = "";
         boolean check = false;
         int j = 0;
-        int row = 3;
+        int row = key;
         int col = message.length();
         char[][] railFenceArray = new char[row][col];
 
         for(int i = 0; i < col; i++) {
-            if(j == 0 || j == 3 -1)
+            if(j == 0 || j == row - 1)
                 check = !check;
 
             railFenceArray[j][i] = message.charAt(i);
@@ -38,12 +44,12 @@ class RailFence implements Cipher {
         String decryptedText = "";
         boolean check = false;
         int j = 0;
-        int row = 3;
+        int row = key;
         int col = message.length();
         char[][] railsFenceArray = new char[row][col];
 
         for(int i = 0; i < col; i++) {
-            if(j == 0 || j == 3 - 1)
+            if(j == 0 || j == key - 1)
                 check = !check;
 
             railsFenceArray[j][i] = '*';
@@ -55,6 +61,7 @@ class RailFence implements Cipher {
         }
 
         int index = 0;
+        check = false;
 
         for(int i = 0; i < row; i++){
             for(int k = 0; k < col; k++){
@@ -62,13 +69,12 @@ class RailFence implements Cipher {
                     railsFenceArray[i][k] = message.charAt(index++);
                 }
             }
-
         }
 
+        j = 0;
         for(int i = 0; i < col; i++) {
-            if(j == 0 || j == 3 - 1)
+            if(j == 0 || j == key - 1)
                 check = !check;
-
             decryptedText += railsFenceArray[j][i];
 
             if(check)
